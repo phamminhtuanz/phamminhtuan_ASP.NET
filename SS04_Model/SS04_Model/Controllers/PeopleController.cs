2bc0df24c3c18280b65c09633adf9430552b785a
phamminhtuanz <162664440+phamminhtuanz@users.noreply.github.com>
2024-11-14 10:33:42 +0700

    14/11/2024

---
+++ SS04_Model/SS04_Model/Controllers/PeopleController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SS04_Model.Models;
+
+namespace SS04_Model.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: PeopleController
+        public ActionResult Index()
+        {
+            var _peoples = DataLocal.GetPeoples();
+            return View(_peoples);
+        }
+
+        // GET: PeopleController/Details/5
+        public ActionResult Details(int id)
+        {
+            var peoples = DataLocal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // GET: PeopleController/Create
+        public ActionResult Create()
+        {
+            People people = new People();
+            return View(people);
+        }
+
+        // POST: PeopleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(People model)
+        {
+            try
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count() > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var FileName = file.Name;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\avatar", FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "images/avatar/" + FileName;
+                    }
+                }
+                DataLocal._peoples.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: PeopleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var people = DataLocal.GetPeopleById(id);
+            return View(people);
+        }
+
+        // POST: PeopleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, People model)
+        {
+            try
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count() > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var FileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\avatar", FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "images/avatar/" + FileName;
+                    }
+                }
+                for (int i = 0; i < DataLocal._peoples.Count; i++)
+                {
+                    if (DataLocal._peoples[i].Id == id)
+                    {
+                        DataLocal._peoples[i] = model;
+                        break;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: PeopleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var peoples = DataLocal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // POST: PeopleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, People model)
+        {
+            try
+            {
+                for (int i = 0;i<DataLocal._peoples.Count;i++)
+                {
+                    if (DataLocal._peoples[i].Id == id)
+                    {
+                        DataLocal._peoples.RemoveAt(i);
+                        break;
+                    }    
+                }    
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
