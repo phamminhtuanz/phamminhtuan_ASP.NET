d0b1180b635d49f9a05d7a05107474690c01d70b
phamminhtuanz <162664440+phamminhtuanz@users.noreply.github.com>
2024-12-13 18:24:43 +0700

    13/12/2024

---
+++ Devxuongmoc/Devxuongmoc/Areas/Admins/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Devxuongmoc.Areas.Admins.Controllers
+{
+    //[Area("admins")]
+    public class DashboardController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Devxuongmoc/Devxuongmoc/Models/Category.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Devxuongmoc.Models;
+
+public partial class Category
+{
+    public int Id { get; set; }
+    [Display(Name = "Tiêu Đề")]
+    public string? Title { get; set; }
+
+    [Display(Name = "Hình Ảnh")]
+    public string? Icon { get; set; }
+    
+
+    public string? MateTitle { get; set; }
+
+    public string? MetaKeyword { get; set; }
+
+    [Display(Name = "Mô Tả")]
+    public string? MetaDescription { get; set; }
+
+    public string? Slug { get; set; }
+
+    [Display(Name = "Đơn ĐH")]
+    public int? Orders { get; set; }
+
+    public int? Parentid { get; set; }
+
+    [Display(Name = "NSX")]
+    public DateTime? CreatedDate { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public string? AdminCreated { get; set; }
+
+    public string? AdminUpdated { get; set; }
+
+    public string? Notes { get; set; }
+
+    public byte? Status { get; set; }
+
+    [Display(Name = "Tình Trạng")]
+    public bool? Isdelete { get; set; }
+}
